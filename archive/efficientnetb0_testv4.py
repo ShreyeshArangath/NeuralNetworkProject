d6073b2751c61fb49eeb502e84846ae8c46246b1
MC <mariclaire.de-guia@ttu.edu>
2022-04-06 14:29:52 -0500

    update steps per epoch and batch size in .fit()

---
+++ archive/efficientnetb0_testv4.py
@@ -0,0 +1,159 @@
+# CATEGORICAL CROSS ENTROPY LOSS FUNCTION
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+# from keras.preprocessing import image
+from keras.applications.efficientnet import EfficientNetB0
+# import tensorflow_addons as tfa
+# from sklearn.metrics import f1_score, precision_score, recall_score
+
+from keras import backend as K
+
+# CHANGE: 
+# imageSize: depends on pretrained model
+# modelName
+# trainingFolder: x5_RGB -> x5
+# testingFolder: x5_RGB -> x5
+# keras.Model(name='FeatureExtraction-<model name>')
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    # print(f"true positives: {true_positives}       possible positives:  {possible_positives}         recall:    {recall}")
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    # print(f"true positives: {true_positives}       predicted positives: {predicted_positives}        precision: {precision}")
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)    
+    f1_score = 2*((precision*recall)/(precision+recall+K.epsilon()))
+    # print(f"f1score:        {f1_score}")
+    return f1_score
+
+# Retrieve training data
+def getDataset(dataFolder, imageSize = (224, 224), batchSize = 32):
+    train_ds = keras.utils.image_dataset_from_directory(
+      dataFolder,
+      seed=123,
+      image_size=imageSize,
+      batch_size=batchSize
+    )
+    return train_ds
+
+# Tune buffer size and efficiency 
+# When do we call this?
+def configurePerformance(train_ds, val_ds): 
+    AUTOTUNE = tf.data.AUTOTUNE
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    return train_ds, val_ds
+
+# def buildModel(dropoutRate, numClasses, inpShape = (224, 224, 3)):
+import pickle
+
+# Create pkl file of the model after the training phase
+def dumpModel(modelName, phase): 
+    # Save the trained model as a pickle string.
+    modelName = "model_" + modelName + "_ " + phase + ".pkl"
+    pickle.dump(model, open(modelName, 'wb'))
+
+EPOCHS = 5
+modelName = "efficientNetB0"
+# Initial layer input shape
+inpShape =  (224, 224, 3)
+trainingFolder = 'data/x5/train/RGB/'
+testingFolder = 'data/x5/test_with_labels/RGB/'
+train_ds = getDataset(trainingFolder)
+val_ds =  getDataset(testingFolder)
+train_ds, val_ds = configurePerformance(train_ds, val_ds)
+
+dropoutRate = 0.2
+numClasses = 7 
+inp = layers.Input(shape=inpShape)
+baseModel = EfficientNetB0(
+    weights = "imagenet",
+    include_top = False,
+    classes = numClasses
+) 
+
+baseModel.trainable = False 
+x = baseModel(inp, training=False)
+x =  layers.GlobalAveragePooling2D(name="avg_pool")(x)
+x = layers.Dropout(dropoutRate, noise_shape=None, seed=None)(x)
+out = layers.Dense(numClasses,activation="softmax", name = "pred")(x)
+model = keras.Model(inp, out, name="FeatureExtraction-B0")
+
+model.summary()
+
+model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate=0.01),
+    loss = tf.keras.losses.CategoricalCrossentropy(),
+    metrics=['accuracy',        
+        precision_m,
+        recall_m,
+        f1_m
+    ]
+)
+
+# Feature extraction without the top layers 
+hist_results = model.fit(
+  train_ds,
+  epochs=EPOCHS,
+  steps_per_epoch=len(train_ds),
+  validation_data=val_ds,
+#   validation_steps=int(0.1*len(val_ds),   
+  batch_size=32
+)
+
+try:
+    dumpModel(modelName, "phase1")
+except Exception:
+    pass
+
+# Fine tuning the Feature Extraction Model 
+baseModel.trainable = True
+for layer in model.layers[1].layers:
+    if isinstance(layer, layers.BatchNormalization):
+      layer.trainable = False
+        
+model.compile(
+    loss = tf.keras.losses.CategoricalCrossentropy(),
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001),
+    metrics=['accuracy',        
+        precision_m,
+        recall_m,
+        f1_m
+    ]
+)
+
+# Train it again 
+hist_results_tuned = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=9,
+  steps_per_epoch=len(train_ds),
+  initial_epoch=hist_results.epoch[-1],
+  batch_size=32
+)
+
+try:
+    dumpModel(modelName, "phase2")
+except Exception:
+    pass
+
+preds = model.predict(val_ds, verbose = 1)
+model.evaluate(val_ds)
+
+# from sklearn.metrics import classification_report
+# import numpy as np
+
+# y_pred = model.predict(val_ds, batch_size=32, verbose=1)
+# y_pred_bool = np.argmax(y_pred, axis=1)
+
+# print(classification_report(y_test, y_pred_bool))
