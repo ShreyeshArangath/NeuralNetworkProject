ecebc2d6f19916fe16999c9b533beb90eea60a33
Shreyesh Arangath <shreyesh@Shreyeshs-MacBook-Pro.local>
2022-04-23 14:37:58 -0500

    added vgg16

--- vgg16.py
+++ vgg16.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
-from keras.applications.densenet import DenseNet121
+# from keras.preprocessing import image
+from keras.applications.efficientnet import EfficientNetB0
+from keras.applications.vgg16 import VGG16
 import tensorflow_addons as tfa
-
 
 from keras import backend as K
 
@@ -50,8 +51,8 @@ def dumpModel(modelName, phase):
     # Save the trained model as a pickle string.
     modelName = "model_" + modelName + "_ " + phase + ".pkl"
     pickle.dump(model, open(modelName, 'wb'))
-
-def getDatasetsByCar(cars):
+    
+def getDatasetsByCar(cars,  imageSize = (224, 224), batchSize = 32):
     train_ds = None 
     val_ds = None 
     for car in cars: 
@@ -68,10 +69,18 @@ def getDatasetsByCar(cars):
     return train_ds, val_ds
 
 EPOCHS = 5
-modelName = "efficientNetB0"
+modelName = "vgg16"
+# Initial layer input shape
 inpShape =  (224, 224, 3)
-cars = ['x5', 'model3', 'hilux']
-train_ds, val_ds = getDatasetsByCar(cars)
+cars = ['x5', 'model3']
+train_ds, val_ds = getDatasetsByCar(cars, batchSize=64)
+
+# trainingFolder = 'data/x5/train/RGB/'
+# testingFolder = 'data/x5/test_with_labels/RGB/'
+# # don't need to pass subset string - datasets already split
+# train_ds = getDataset(trainingFolder, "training")
+# val_ds =  getDataset(testingFolder, "validation")
+
 train_ds, val_ds = configurePerformance(train_ds, val_ds)
 
 resize_and_rescale = tf.keras.Sequential([
@@ -87,10 +96,8 @@ data_augmentation = tf.keras.Sequential([
 dropoutRate = 0.2
 numClasses = 7 
 inp = layers.Input(shape=inpShape)
-
-baseModel = DenseNet121(weights="imagenet",
+baseModel = VGG16(weights="imagenet",
                    include_top = False) 
-
 augmentation = tf.keras.Sequential([
   # Add the preprocessing layers you created earlier.
   resize_and_rescale,
@@ -144,6 +151,7 @@ hist_results_tuned = model.fit(
   train_ds,
   validation_data=val_ds,
   epochs=9,
+  #steps_per_epoch=len(train_ds)?
   initial_epoch=hist_results.epoch[-1]
 )
 
@@ -151,5 +159,3 @@ dumpModel(modelName, "phase2")
 
 preds = model.predict(val_ds, verbose = 1)
 model.evaluate(val_ds)
-
-
