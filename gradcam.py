dbc2f0faeae80812616cbb8de6c25e8c44b3ec3e
Rayven <rjderay@gmail.com>
2022-05-03 22:05:55 -0500

    added vgg16. have to still add datasets

--- gradcam.py
+++ gradcam.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from keras.applications.vgg16 import (
     VGG16, preprocess_input, decode_predictions)
 from keras.preprocessing import image
@@ -79,11 +80,69 @@ def f1_m(y_true, y_pred):
     recall = recall_m(y_true, y_pred)
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
+def configurePerformance(train_ds, val_ds): 
+    AUTOTUNE = tf.data.AUTOTUNE
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    return train_ds, val_ds
+
+def getDatasetsByCar(cars,  imageSize = (224, 224), batchSize = 32):
+    train_ds = None 
+    val_ds = None 
+    for car in cars: 
+        trainingFolder = 'data/'+ car +'/train/RGB/'
+        testingFolder = 'data/'+ car + '/test_with_labels/RGB/'
+        if not train_ds and not val_ds:
+            train_ds = getDataset(trainingFolder, "training")
+            val_ds =  getDataset(testingFolder, "validation")
+        else: 
+            new_train_ds =  getDataset(trainingFolder, "training")
+            new_val_ds =  getDataset(testingFolder, "validation")
+            train_ds.concatenate(new_train_ds)
+            val_ds.concatenate(new_val_ds)
+    return train_ds, val_ds
+
+def getDataset(dataFolder, subset, imageSize = (224, 224), batchSize = 32):
+    train_ds = keras.utils.image_dataset_from_directory(
+      dataFolder,
+      seed=123,
+      image_size=imageSize,
+      batch_size=batchSize)
+    return train_ds
+
+
+EPOCHS = 5
+modelName = "vgg16"
 inpShape =  (224, 224, 3)
+cars = ['x5', 'model3']
+train_ds, val_ds = getDatasetsByCar(cars, batchSize=64)
+
+train_ds, val_ds = configurePerformance(train_ds, val_ds)
+
+
+resize_and_rescale = tf.keras.Sequential([
+  layers.Resizing(inpShape[0], inpShape[0]),
+  layers.Rescaling(1./255)
+])
+
+data_augmentation = tf.keras.Sequential([
+  layers.RandomFlip("horizontal_and_vertical"),
+  layers.RandomRotation(0.2),
+])
+
 dropoutRate = 0.2
 numClasses = 7 
 inp = layers.Input(shape=inpShape)
 baseModel = VGG16(weights='imagenet')
+
+augmentation = tf.keras.Sequential([
+  # Add the preprocessing layers you created earlier.
+  resize_and_rescale,
+  data_augmentation,
+])
+
+inp = augmentation(inp)
+
 
 baseModel.trainable = False 
 x = baseModel(inp, training=False)
@@ -101,10 +160,17 @@ model.compile(loss = tf.keras.losses.SparseCategoricalCrossentropy(),
               ]
               )
 
+# Train it again 
+hist_results_tuned = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=9,
+
+)
 
 input = load_image(sys.argv[1])
 print(model.summary())
-preds = model.predict(input)
+preds = model.predict(val_ds, verbose=1)
 print(preds)
 top_1 = np.argmax(preds)
 print("Predicted:", top_1)
